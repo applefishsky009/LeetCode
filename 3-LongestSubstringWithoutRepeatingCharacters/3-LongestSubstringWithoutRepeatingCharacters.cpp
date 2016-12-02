4043c499783283de4ba3129f1d1bafa759c60411
ryl <ryl_public@163.com>
2016-12-02 16:53:04 +0800

    Hash Table, Two Pointers, String

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//dvdf返回3，注意从上一个重复字符的下一个字符计数
+class Solution
+{
+  public:
+    int lengthOfLongestSubstring(string s)
+    {
+		string temp = "";
+		int maxLen = 0; //变量要初始化，否则在一些编译器上会出错
+		for (int i = 0; i < s.size(); ++i)
+		{
+			char tempOne = s[i];
+			size_t locate = temp.find(tempOne);
+			if (locate != string::npos)
+			{
+				int len = temp.size();
+				maxLen = max(maxLen, len);
+				temp = temp.substr(locate + 1); //从上一个重复字符的下一个字符计数
+			}
+			temp.push_back(tempOne);
+		}
+		int len = temp.size();
+		maxLen = max(maxLen, len); //最后一次
+		return maxLen;
+    }
+};
+
+int main()
+{
+    string s = "abcabcbb";
+    Solution s1;
+    cout << s1.lengthOfLongestSubstring(s) << endl;
+    return 0;
+}
