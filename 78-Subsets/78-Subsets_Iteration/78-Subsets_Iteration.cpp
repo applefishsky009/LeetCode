ec309ed57188860866d1ea4d7075a939440ebc38
ryl <ryl_public@163.com>
2016-12-05 10:57:34 +0800

    Array, Backtracking, Bit Manipulation

---
+++ 78-Subsets/78-Subsets_Iteration/78-Subsets_Iteration.cpp
@@ -0,0 +1,34 @@
+﻿#include <vector>
+#include <algorithm>
+#include <E:\RylModule\vector.h>
+using namespace std;
+
+//二进制加法原理
+class Solution
+{
+  public:
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+			sort(nums.begin(), nums.end());
+			vector<vector<int>> result;
+			vector<int> v;
+			for (int i = 0; i < 1 << nums.size(); i++) //子数组一共 1<<nums.size()个，采用二进制逐个选择
+			{
+				for (int j = 0; j < nums.size(); j++) //判断每一位要不要选择
+				if (i & 1 << j)
+					v.push_back(nums[j]); //i&1<<j 可以判断二进制中j位到底有没有
+				result.push_back(v);
+				v.clear();
+			}
+			return result;
+    }
+};
+
+int main()
+{
+    Solution s1;
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> result = s1.subsets(nums); //结果按照二进制排序
+    show2DVector(result);
+    return 0;
+}
---
+++ 78-Subsets/78-Subsets_Recursion/78-Subsets_Recursion.cpp
@@ -0,0 +1,117 @@
+﻿#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm> //vector的sort要用
+#include <E:\RylModule\vector.h>
+using namespace std;
+
+class Solution
+{
+  public:
+    vector<vector<int>> subsets(vector<int> &S)
+    {
+		sort(S.begin(), S.end()); //输出要求有序
+		vector<vector<int>> result;
+		vector<int> path;
+		subsets(S, path, 0, result);
+		return result;
+    }
+
+  private:
+    static void subsets(const vector<int> &S, vector<int> &path, int step,
+			vector<vector<int>> &result)
+    {
+		if (step == S.size())
+		{
+			result.push_back(path);
+			return;
+		}
+		//不??? S[step]
+		subsets(S, path, step + 1, result);
+		//??? S[step]
+		path.push_back(S[step]);
+		subsets(S, path, step + 1, result);
+		path.pop_back();
+    }
+};
+
+class Solution1
+{
+  public:
+    vector<vector<int>> subsets(vector<int> &S)
+    {
+		sort(S.begin(), S.end()); //输出要求有序
+		vector<vector<int>> result;
+		vector<bool> selected(S.size(), false);
+		subsets(S, selected, 0, result);
+		return result;
+    }
+
+  private:
+    static void subsets(const vector<int> &S, vector<bool> &selected, int step,
+			vector<vector<int>> &result)
+    {
+		if (step == S.size())
+		{
+			vector<int> subset;
+			for (int i = 0; i < S.size(); i++)
+				if (selected[i])
+					subset.push_back(S[i]);
+			result.push_back(subset);
+			return;
+		}
+		//??? S[step]
+		selected[step] = false;
+		subsets(S, selected, step + 1, result);
+		//不??? S[step]
+		selected[step] = true;
+		subsets(S, selected, step + 1, result);
+    }
+};
+
+int main()
+{
+	cout << "hello world" << endl;
+    int idex = 0;
+    const int num = 3;
+    while (1)
+	{
+		cout << "there is two recursion method, please choose one of them." << endl;
+		cout << "1. Increment Construction Method." << endl;
+		cout << "2. Bit Vector Method." << endl;
+		cout << "which one would you choose?   ";
+		while (!(cin >> idex))
+		{
+			cin.clear();
+			while (!cin.get() != '\n')
+				continue;
+			cout << "请重新输入：" << endl;
+		}
+		switch (idex)
+		{
+		case 1:
+		{	//在代码块内初始化局部变???
+			vector<int> sIn1 = {1, 2, 3};
+			vector<vector<int>> sOut1(pow(num, 2), vector<int>(num, 0));
+			Solution x;
+			sOut1 = x.subsets(sIn1);
+			show2DVector(sOut1);
+		}
+			break;
+		case 2:
+		{
+			vector<int> sIn2 = {1, 2, 3};
+			vector<vector<int>> sOut2(pow(num, 2), vector<int>(num, 0));
+			Solution1 x1;
+			sOut2 = x1.subsets(sIn2);
+			show2DVector(sOut2);
+		}
+			break;
+		default:
+			cout << endl;
+			break;
+		}
+		cout << endl;
+    }
+    return 0;
+}
