81b281d64a2d928cd00b6e78bdee1c0294f34fe8
ryl <ryl_public@163.com>
2016-12-05 10:48:01 +0800

    Array, Greedy

---
+++ 55-JumpGame/55-JumpGame.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+//自底向上的迭代贪心，自顶向下省略。
+class Solution
+{
+  public:
+    bool canJump(vector<int> &nums)
+    {
+		int reach = 0;
+		for (int i = 0; i < nums.size() - 1; i++)			     //注意最后一步就别跳了
+			if (reach >= i)						     //能不能跳到当前步？
+				reach = (i + nums.at(i)) > reach ? (i + nums.at(i)) : reach; //更新最优解
+		return reach >= nums.size() - 1;
+    }
+};
+
+int main()
+{
+    vector<int> numIn = {0, 2, 3};
+    Solution s;
+    bool isJump = s.canJump(numIn);
+    cout << isJump << endl;
+    return 0;
+}
