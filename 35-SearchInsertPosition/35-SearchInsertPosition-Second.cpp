395d3eb948c210de0021d3b9f86e638eb4950505
applefishsky009 <ryl_public@163.com>
2020-07-17 08:10:25 +0800

    Array, BS

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition-Second.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+// 经典二分
+class Solution {
+  public:
+	int searchInsert(vector<int> &nums, int target) {
+		int l = 0, r = nums.size() - 1;
+		while (l < r) {
+			int mid = l + ((r - l) >> 1);
+			if (nums[mid] < target)
+				l = mid + 1;
+			else	// 找大于等于target的第一个数
+				r = mid;
+		}
+		return nums[l] >= target ? l : l + 1;	// 是否找到大于等于target的第一个数
+	}
+};
+
+int main() {
+	Solution s;
+	vector<int> nums = {1, 3, 5, 6};
+	cout << s.searchInsert(nums, 5) << endl;
+	cout << s.searchInsert(nums, 2) << endl;
+	cout << s.searchInsert(nums, 7) << endl;
+	cout << s.searchInsert(nums, 0) << endl;
+	return 0;
+}
