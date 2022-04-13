0ba0aaf5086ebf9c8912a7a26a8a75b6c0287bea
ryl <ryl_public@163.com>
2022-04-13 17:37:34 +0800

    Database
    Database

---
+++ 1179-ReformatDepartmentTable/1179-ReformatDepartmentTable.sql
@@ -0,0 +1,18 @@
+-- https://leetcode-cn.com/problems/reformat-department-table/solution/guan-yu-group-byyu-sumde-pei-he-by-xxiao053/
+-- 当一个单元格中有多个数据时，case when只会提取当中的第一个数据。
+SELECT id, 
+SUM(CASE WHEN month='Jan' THEN revenue END) AS Jan_Revenue,
+SUM(CASE WHEN month='Feb' THEN revenue END) AS Feb_Revenue,
+SUM(CASE WHEN month='Mar' THEN revenue END) AS Mar_Revenue,
+SUM(CASE WHEN month='Apr' THEN revenue END) AS Apr_Revenue,
+SUM(CASE WHEN month='May' THEN revenue END) AS May_Revenue,
+SUM(CASE WHEN month='Jun' THEN revenue END) AS Jun_Revenue,
+SUM(CASE WHEN month='Jul' THEN revenue END) AS Jul_Revenue,
+SUM(CASE WHEN month='Aug' THEN revenue END) AS Aug_Revenue,
+SUM(CASE WHEN month='Sep' THEN revenue END) AS Sep_Revenue,
+SUM(CASE WHEN month='Oct' THEN revenue END) AS Oct_Revenue,
+SUM(CASE WHEN month='Nov' THEN revenue END) AS Nov_Revenue,
+SUM(CASE WHEN month='Dec' THEN revenue END) AS Dec_Revenue
+FROM department
+GROUP BY id
+ORDER BY id;
